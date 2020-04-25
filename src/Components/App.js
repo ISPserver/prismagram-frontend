be04e0750a52b9b510120ce9f5fd9551988f9ead
ISPserver <tjdals080@naver.com>
2020-04-25 22:36:38 +0900

    First Hooks Query

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { gql } from "apollo-boost";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from '../Styles/GlobalStyles';
 import Router from './Router';
 import Theme from '../Styles/Theme';
+import { useQuery } from 'react-apollo-hooks';
 
-export default () => (
-  <ThemeProvider theme={Theme}>
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  
+  const { 
+    data : { isLoggedIn} 
+  } = useQuery(QUERY);  
+
+  return (
+    <ThemeProvider theme={Theme}>
     <>
       <GlobalStyles />
-      <Router isLoggedIn={false} />
+      <Router isLoggedIn={isLoggedIn} />
     </>
   </ThemeProvider>
-);
+  )
+};
+ 
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "react-style-reset";
 //styled-components의 최신방식으로 만드는 global styles
+
 export default createGlobalStyle`
     ${reset};
     * {
         box-sizing:border-box;
+    }
+    body {
+        background-color:${props => props.theme.bgColor};
+        color:${props => props.theme.blackColor};
+    }
+    a {
+        color:${props => props.theme.blueColor};
+        text-decoration:none;
     }
 `;
