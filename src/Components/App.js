931b8fc0de2965f13886dbe3c79acb408242ad4e
ISPserver <tjdals080@naver.com>
2020-04-29 18:35:59 +0900

    Auth Route UI part One

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,10 +8,16 @@ import { useQuery } from 'react-apollo-hooks';
 
 const QUERY = gql`
   {
-    isLoggedIn @client
+    isLoggedIn @client    
   }
 `;
-
+/*상단에 @client 쓴 이유
+GraphQL 백엔드 서버에서 가져올 데이터가 아니라는 것을 @client라고 써서 알려주는 것.
+대신 이 앱을 사용하는 사용자의 로컬 캐시(cache)에서 참조.
+cache.writeData({ data... })으로 생성한 것.
+because 서버에 계속 참조 요청을 보내면 돈을 더 내야하기 때문에
+한번만 사용자의 로컬 캐시에 저장해두고 그 isLoggedIn 데이터는 
+로컬에서만 참조하니까 서버에는 부하가 전혀 없게되는것 */
 export default () => {
   
   const { 
