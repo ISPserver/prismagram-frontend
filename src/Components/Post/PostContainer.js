392e003838e56fa410c839330bd0852a6d133a5d
ISPserver <tjdals080@naver.com>
2020-06-14 23:12:49 +0900

    createComment on Post Component

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -2,8 +2,10 @@ import React, {useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
-import { useMutation } from "react-apollo-hooks";
+import { useMutation, useQuery } from "react-apollo-hooks";
 import { TOGGLE_LIKE, ADD_COMMENT } from "./PostQueries";
+import { ME } from "../../SharedQueries";
+import { toast } from "react-toastify";
 
 const PostContainer = ({
     id, 
@@ -19,7 +21,8 @@ const PostContainer = ({
     const [isLikedS, setIsLiked] = useState(isLiked);
     const [likeCountS, setLikeCount] = useState(likeCount);
     const [currentItem, setCurrentItem] = useState(0);
-    const comment = useInput("");
+    const [selfComments, setSelfComments] = useState([]);
+    const comment = useInput("");    
     const [toggleLikeMutation] = useMutation(TOGGLE_LIKE, 
         {variables: {postId: id}
     });
@@ -48,6 +51,21 @@ const PostContainer = ({
             setLikeCount(likeCountS +1);
         }      
     };
+    const onKeyPress = async event => {
+        const { which } = event;
+        if(which === 13){            
+            event.preventDefault();
+            try{
+                const {
+                    data: { addComment }
+                } = await addCommentMutation();
+                setSelfComments([...selfComments, addComment]);
+                comment.setValue("");
+            } catch {
+                toast.error("Can't send comment");
+            }
+        }
+    };
 
     return (
     <PostPresenter 
@@ -64,6 +82,8 @@ const PostContainer = ({
         setLikeCount={setLikeCount}
         currentItem={currentItem}
         toggleLike={toggleLike}
+        onKeyPress={onKeyPress}
+        selfComments={selfComments}
     />
     );
 };
